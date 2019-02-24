36e4e146a17e8331532e137c96d34b585b5d5e58
pauli <pauli@LAPTOP-8B2RBED0.Home>
2019-02-24 13:21:13 -0700

    Initial commit of the assignment 2 / calculator project

---
+++ Assignment_2/src/cse360assign2/Calculator.java
@@ -0,0 +1,81 @@
+package cse360assign2;
+/**
+ * 
+ * Name: Paulina Limon
+ * Class ID: 456
+ * Assignment Number: 2
+ * 
+ * Calculator
+ * This class defines a calculator that takes values and calculates
+ * them into the integer total. 
+ * The user will be able to do the basic arithmetic (such as adding,
+ * subtracting, multiplying or dividing) to a total number. 
+ * Additionally will be able to see the result of all the calculations
+ * or get the history of all the actions done to the calcultor.
+ *
+ */
+
+public class Calculator {
+
+	private int total;
+	/**
+	 * Class constructor
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	/**
+	 * 
+	 * getTotal returns the total integer after all the 
+	 * calculations done before calling it.
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	
+	/**
+	 * Adds the parameter to the total variable.
+	 * @param value: number given by the user to add.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	
+	/**
+	 * Subtracts the parameter from the total variable.
+	 * @param value: number given by the user to subtract.
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	
+	/**
+	 * Multiplies the total by the parameter.
+	 * @param value: number given by the user to multiply.
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	
+	/**
+	 * Divides the total by the parameter.
+	 * @param value: number given by the user to divide.
+	 * If the number is zero, it sets the total equal to zero
+	 * in order to avoid any exceptions displayed.
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	
+	/**
+	 * getHistory returns the actions done by the user.
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
